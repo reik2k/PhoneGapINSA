346bead8e78fec79ea92178d37964d0ee94d5aa2
abel2k <reik2k@hotmail.com>
2014-10-29 18:34:31 +0100

    T4EJ1.0 CONECTION: Agregar ficheros del plugin>src>ios a el Proyecto..Platforms>myAppINSA>Plugins

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,8 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady(), false);
+        
     },
     // deviceready Event Handler
     //
@@ -34,16 +35,36 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.checkConnection();
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    receivedEvent: function(id) 
+    {
+        var parentElement       = document.getElementById(id);
+        var listeningElement    = parentElement.querySelector('.listening');
+        var receivedElement     = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
+        receivedElement.setAttribute('style', 'display:none;');
+        
         console.log('Received Event: ' + id);
+    },
+    
+    checkConnection:function()
+    {
+        var networkState    = navigator.connection.type;
+        
+        var states          = {};
+        
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+        
+        alert('Connection type: ' + states[networkState]);
     }
 };
